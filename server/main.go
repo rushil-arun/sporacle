d75e3b4c08b98be0c5a1019e36037b4a0b002d06
rushil-arun <rushilarun@gmail.com>
2026-02-03 23:59:08 -0800

    Adding CORS handling

--- server/main.go
+++ server/main.go
@@ -9,6 +9,19 @@ import (
 	state "server/state"
 )
 
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	fmt.Println("Welcome to Sporcle!")
 
